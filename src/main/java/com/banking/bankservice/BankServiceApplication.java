640522b9b9078f392848ae2485b326e33303a410
Jesus Alvarez Gonzalez <thok54@Jesuss-MacBook-Pro.local>
2019-09-15 19:55:21 +0200

    first commit

---
+++ src/main/java/com/banking/bankservice/BankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking.bankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankServiceApplication.class, args);
+	}
+
+}
