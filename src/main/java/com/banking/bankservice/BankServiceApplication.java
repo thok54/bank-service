ffe1e2cd1498af473048e63827bf038c5f2fc047
Jesus Alvarez Gonzalez <thok54@Jesuss-MacBook-Pro.local>
2019-09-17 11:18:09 +0200

    gitignore fix

--- src/main/java/com/banking/bankservice/BankServiceApplication.java
+++ src/main/java/com/banking/bankservice/BankServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankServiceApplication {
-	//TODO: bank-service, account-service, bank-account. Please work
+	//TODO: bank-service, account-service, bank-account.
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankServiceApplication.class, args);
