67a188fbf743b1e5b76212726ab4a9bc9e8a204e
jantpedraza <jantpedraza@gmail.com>
2019-09-22 19:37:05 +0200

    Use spring.datasource properties

--- src/main/java/com/banking/bankservice/BankServiceApplication.java
+++ src/main/java/com/banking/bankservice/BankServiceApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankServiceApplication {
-	//TODO: bank-service, account-service, bank-account.
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankServiceApplication.class, args);
 	}
-
 }
