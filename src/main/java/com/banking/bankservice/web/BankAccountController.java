225a9b62e8cd7de99f48bb3203e1302961b16db7
Jesus Alvarez <thok54@hotmail.com>
2019-09-25 09:59:53 +0200

    bank-account restTemplate is weird

--- src/main/java/com/banking/bankservice/web/BankAccountController.java
+++ src/main/java/com/banking/bankservice/web/BankAccountController.java
@@ -2,10 +2,11 @@ package com.banking.bankservice.web;
 
 import com.banking.bankservice.dto.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -17,8 +18,29 @@ public class BankAccountController {
 
     @GetMapping
     public Account get() {
-        ResponseEntity<Account> response = restTemplate.getForEntity("http://localhost:8090/account", Account.class);
-
+        ResponseEntity<Account> response = restTemplate.getForEntity("http://localhost:8085/account", Account.class);
         return response.getBody();
+    }
+
+    @PostMapping
+    public void post() {
+        HttpEntity<Account> request = new HttpEntity<>(new Account());
+        restTemplate.postForEntity("http://localhost:8085/account", request, Account.class);
+    }
+
+    @PutMapping
+    public void put() {/*
+        Account updatedInstance = new Account();
+        updatedInstance.setId(createResponse.getBody().getId());
+        String resourceUrl = "http://localhost:8085/account";
+        HttpEntity<Account> requestUpdate = new HttpEntity<>(updatedInstance, headers);
+        template.exchange(resourceUrl, HttpMethod.PUT, requestUpdate, Void.class);
+        return response.getBody();
+    */
+    }
+
+    @DeleteMapping
+    public void delete() {
+        restTemplate.delete("http://localhost:8085/account");
     }
 }
