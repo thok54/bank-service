8c4d73e38ae800da677a1acbc8d63b6bc9fc34fb
Jesus Alvarez <thok54@hotmail.com>
2019-09-29 22:36:59 +0200

    BankAccountController and Service(not working yet )

--- src/main/java/com/banking/bankservice/web/BankAccountController.java
+++ src/main/java/com/banking/bankservice/web/BankAccountController.java
@@ -1,6 +1,7 @@
 package com.banking.bankservice.web;
 
 import com.banking.bankservice.dto.Account;
+import com.banking.bankservice.service.BankAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,52 +24,52 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 @RequestMapping("/bank/account")
 public class BankAccountController {
 
-    //TODO:BankAccount Controller + BankAccountService.
+    //TODO: BankAccountService, BankService and AccountService tests.
 
     private String basePath = "/account";
 
     @Autowired
-    private RestTemplate accountRestTemplate;
+    BankAccountService bankAccountService;
 
     @GetMapping
     public List<Account> getAll() {
-        ResponseEntity<Account[]> response = accountRestTemplate.getForEntity(basePath, Account[].class);
+        ResponseEntity<Account[]> response = bankAccountService.findAll();
         return Arrays.asList(response.getBody());
     }
 
     @GetMapping("/{id}")
     public Account getById(@PathVariable Integer id) {
-        ResponseEntity<Account> response = accountRestTemplate.getForEntity(basePath + "/" + id, Account.class);
+        ResponseEntity<Account> response = bankAccountService.find(id);
         return response.getBody();
     }
 
     @GetMapping("/byName/{name}")
-    public Account getByName(@PathVariable String name) {
-        ResponseEntity<Account> response = accountRestTemplate.getForEntity(basePath + "/byName/" + name, Account.class);
-        return response.getBody();
+    public List<Account> getByName(@PathVariable String name) {
+        ResponseEntity<Account[]> response = bankAccountService.findAllByName(name);
+        return Arrays.asList(response.getBody());
     }
 
     @PostMapping
     @ResponseStatus(CREATED)
     public void post(@RequestBody Account acc) {
-        accountRestTemplate.postForEntity(basePath, acc, Account.class);
+        bankAccountService.store(acc);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(NO_CONTENT)
     public void put(@PathVariable Integer id, @RequestBody Account acc) {
-        accountRestTemplate.put(basePath + "/" + id, acc);
+        bankAccountService.update(id, acc);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(NO_CONTENT)
     public void delete(@PathVariable Integer id) {
-        accountRestTemplate.delete(basePath + "/" + id);
+        bankAccountService.delete(id);
     }
 
     @PutMapping("/reset")
     @ResponseStatus(NO_CONTENT)
     public void reset(@RequestBody Account acc) {
-        accountRestTemplate.put(basePath + "/reset", acc);
+        bankAccountService.reset(acc);
     }
 }
