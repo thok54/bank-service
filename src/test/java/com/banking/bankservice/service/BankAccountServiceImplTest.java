3adcd6260dfb02d81d03ef4562adb4bc3f45ceee
Jesus Alvarez <thok54@hotmail.com>
2019-10-29 11:00:15 +0100

    BankAccountServiceImpl test

--- src/test/java/com/banking/bankservice/service/BankAccountServiceImplTest.java
+++ src/test/java/com/banking/bankservice/service/BankAccountServiceImplTest.java
@@ -1,38 +1,73 @@
 package com.banking.bankservice.service;
 
+import com.banking.bankservice.dto.Account;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.client.RestTemplate;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BankAccountServiceImplTest {
 
-    //TODO: Complete tests, expressions, exercises.
+    private String basePath = "/account";
+    private static final String ACCOUNT_NAME = "name";
+    private static final int ACCOUNT_ID = 1;
+    private Account expectedAccount = new Account();
+
+    @Mock
+    private RestTemplate accountRestTemplate;
+
+    @InjectMocks
+    private BankAccountServiceImpl bankAccountService;
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
     @Test
-    public void findAll() {
+    public void findAllCallsSuccesfully() {
+        bankAccountService.findAll();
+        verify(accountRestTemplate).getForEntity(basePath, Account[].class);
     }
 
     @Test
-    public void find() {
+    public void findCallsSuccesfully() {
+        bankAccountService.find(ACCOUNT_ID);
+        verify(accountRestTemplate).getForEntity(basePath + "/" + ACCOUNT_ID, Account.class);
     }
 
     @Test
-    public void findAllByName() {
+    public void findAllByNameCallsSuccesfully() {
+        bankAccountService.findAllByName(ACCOUNT_NAME);
+        verify(accountRestTemplate).getForEntity(basePath + "/byName/" + ACCOUNT_NAME, Account[].class);
     }
 
     @Test
-    public void store() {
+    public void storeCallsSuccesfully() {
+        bankAccountService.store(expectedAccount);
+        verify(accountRestTemplate).postForEntity(basePath, expectedAccount, Account.class);
     }
 
     @Test
-    public void update() {
+    public void updateCallsSuccesfully() {
+        bankAccountService.update(ACCOUNT_ID, expectedAccount);
+        verify(accountRestTemplate).put(basePath + "/" + ACCOUNT_ID, expectedAccount);
     }
 
     @Test
-    public void delete() {
+    public void deleteCallsSuccesfully() {
+        bankAccountService.delete(ACCOUNT_ID);
+        verify(accountRestTemplate).delete(basePath + "/" + ACCOUNT_ID);
     }
 
     @Test
-    public void reset() {
+    public void resetCallsSuccesfully() {
+        bankAccountService.reset(expectedAccount);
+        verify(accountRestTemplate).put(basePath + "/reset", expectedAccount);
     }
 }
